86f8e56e0359e489a73bf9a67a0a71e4de8ed4d6
kevinhes <kevinhes1115@gmail.com>
2024-09-16 13:18:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<template>
+  <main class="main-section">
+    <section class="todo-list-section">
+      <section class="sidebar-menu">
+        <div class="position-relative">
+          <i class="bi bi-search text-light position-absolute top-50 start-2 translate-middle-y"></i>
+          <input type="text" class="form-control form-control-search mb-5" placeholder="搜尋">
+        </div>
+        <ul class="grid gap-3 list-unstyled">
+          <li class="g-col-6">1</li>
+          <li class="g-col-6">2</li>
+          <li class="g-col-6">3</li>
+          <li class="g-col-6">4</li>
+        </ul>
+      </section>
+      <section class="todolist"></section>
+    </section>
+  </main>
+</template>
+
